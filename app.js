b006965cf7c6b06fcb040c8656681cec45480801
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-06 16:38:26 +0500

    log commit

--- app.js
+++ app.js
@@ -27,8 +27,6 @@ app.get("/webhook", (req, res) => {
 app.post("/webhook", (req, res) => {
   let body_param = req.body;
 
-  console.log(JSON.stringify(body_param, null, 2));
-
   if (body_param.object) {
     if (
       body_param.entry &&
@@ -42,7 +40,8 @@ app.post("/webhook", (req, res) => {
       let mesg_body =
         body_param.entry[0].changes[0].value.messages[0].text.body;
 
-      axios({
+      console.log("USer send this message", mesg_body);
+      /**    axios({
         method: "POST",
         url:
           "https://graph.facebook.com/v18.0/" +
@@ -59,7 +58,7 @@ app.post("/webhook", (req, res) => {
         headers: {
           "Content-Type": "application/json",
         },
-      });
+      });**/
       res.sendStatus(200);
     } else {
       res.sendStatus(403);
