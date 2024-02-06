4ede2d85d43942dcba517599722d511ba14167a3
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-06 16:50:25 +0500

    err commit

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.get("/webhook", (req, res) => {
 app.post("/webhook", (req, res) => {
   let body_param = req.body;
 
+  console.log(JSON.stringify(body_param, null, 2));
+
   if (body_param.object) {
     if (
       body_param.entry &&
@@ -40,8 +42,7 @@ app.post("/webhook", (req, res) => {
       let mesg_body =
         body_param.entry[0].changes[0].value.messages[0].text.body;
 
-      console.log("USer send this message", mesg_body);
-      /**    axios({
+   /**    axios({
         method: "POST",
         url:
           "https://graph.facebook.com/v18.0/" +
