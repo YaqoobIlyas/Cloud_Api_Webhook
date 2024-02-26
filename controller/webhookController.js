1e54e9dba7775a75fc362cb92a381304d3f3ada0
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-26 21:54:55 +0500

    finalResul9  commit

--- controller/webhookController.js
+++ controller/webhookController.js
@@ -46,16 +46,32 @@ exports.webhookEndpoint = async (req, res) => {
           const apiUrl = `http://tanzeemulmadaris.net/Home/ShowResult?RollNo=${mesg_body}`;
           const response = await axios.get(apiUrl);
           const resultData = response.data;
-          const formattedResult = resultData.Result.replace(/\n/g, " ");
+          // Extract individual scores from the response
+          const scores = {
+            P1: resultData.P1,
+            P2: resultData.P2,
+            P3: resultData.P3,
+            P4: resultData.P4,
+            P5: resultData.P5,
+            P6: resultData.P6,
+            PartFirst: resultData.PartFirst,
+            MarksObtained: resultData.MarksObtained,
+            Grade: resultData.Grade,
+            ExamStatus: resultData.ExamStatus,
+          };
+          let formattedResult = "";
+          Object.keys(scores).forEach((key) => {
+            formattedResult += `${key}: ${scores[key]}\n`;
+          });
           console.log("Result: ", formattedResult);
 
-          const fbResponse = await axios.post(
+          await axios.post(
             `https://graph.facebook.com/v18.0/${phon_no_id}/messages?access_token=${token}`,
             {
               messaging_product: "whatsapp",
               to: from,
               text: {
-                body: "غیرحاضر",
+                body: formattedResult,
               },
             }
           );
