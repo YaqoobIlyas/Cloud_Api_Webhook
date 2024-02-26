9806781f98e9f297b15c254849758783ceb7ae4a
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-26 15:30:15 +0500

    finalResult1 commit

--- controller/webhookController.js
+++ controller/webhookController.js
@@ -45,7 +45,7 @@ exports.webhookEndpoint = async (req, res) => {
         try {
           const apiUrl = `http://tanzeemulmadaris.net/Home/ShowResult?RollNo=${mesg_body}`;
           const response = await axios.get(apiUrl);
-          const resultData = response.data.Result;
+          const resultData = response.data;
           console.log("Result: ", resultData);
           // Extract individual scores from the response
           const scores = {
