590a989f2a47178fc7349263851a54a1112ca3bf
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-29 14:52:09 +0500

    autowhats1  commit

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,6 +1,8 @@
 exports.autoMessage = async (req, res) => {
   let body_param = req.body;
   console.log(body_param);
+  console.log("Message: ", body_param.message);
+
   res.json({
     reply: "Hi, we have received your request. Thanks for contacting.",
   });
