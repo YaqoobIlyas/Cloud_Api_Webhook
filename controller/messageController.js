6bb21f75995713eb2edc4f911a4ae438e8d25302
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-29 18:15:54 +0500

    autowhats100  commit

--- controller/messageController.js
+++ controller/messageController.js
@@ -5,38 +5,9 @@ exports.autoMessage = async (req, res) => {
   let formattedResult = ""; // Default value
   console.log(body_param);
   console.log("Message: ", body_param.message);
-
-  try {
-    const apiUrl = `http://tanzeemulmadaris.net/Home/ShowResult?RollNo=${body_param.message}`;
-    const response = await axios.get(apiUrl);
-    const resultData = response.data;
-
-    // Extract individual scores from the response
-    const scores = {
-      P1: resultData.P1,
-      P2: resultData.P2,
-      P3: resultData.P3,
-      P4: resultData.P4,
-      P5: resultData.P5,
-      P6: resultData.P6,
-      PartFirst: resultData.PartFirst,
-      MarksObtained: resultData.MarksObtained,
-      Grade: resultData.Grade,
-      ExamStatus: resultData.ExamStatus,
-    };
-
-    formattedResult = "";
-    Object.keys(scores).forEach((key) => {
-      formattedResult += `${key}: ${scores[key]}\n`;
-    });
-    console.log("Result: ", formattedResult);
-  } catch (error) {
-    console.error("Error:", error);
-    formattedResult = "Error occurred. Please try again."; // Update formattedResult in case of error
-  }
-
-  // Send response outside the try-catch block
-  res.json({
-    reply: formattedResult,
-  });
+ res.json({
+   reply: formattedResult,
+ });
+  
+ 
 };
