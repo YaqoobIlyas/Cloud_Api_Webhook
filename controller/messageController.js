886131a6a624c9aaebc0981708316809c56f3fd3
Yaqoob Ilyas <yaqoobilyas786@gmail.com>
2024-02-29 16:24:34 +0500

    autowhats4  commit

--- controller/messageController.js
+++ controller/messageController.js
@@ -3,7 +3,38 @@ exports.autoMessage = async (req, res) => {
   console.log(body_param);
   console.log("Message: ", body_param.message);
 
-  res.json({
-    reply: "Hi, we have received your request. Thanks for contacting.",
-  });
+  try {
+    const apiUrl = `http://tanzeemulmadaris.net/Home/ShowResult?RollNo=${body_param.message}`;
+    const response = await axios.get(apiUrl);
+    const resultData = response.data;
+    // Extract individual scores from the response
+    const scores = {
+      P1: resultData.P1,
+      P2: resultData.P2,
+      P3: resultData.P3,
+      P4: resultData.P4,
+      P5: resultData.P5,
+      P6: resultData.P6,
+      PartFirst: resultData.PartFirst,
+      MarksObtained: resultData.MarksObtained,
+      Grade: resultData.Grade,
+      ExamStatus: resultData.ExamStatus,
+    };
+    let formattedResult = "";
+    Object.keys(scores).forEach((key) => {
+      formattedResult += `${key}: ${scores[key]}\n`;
+    });
+    console.log("Result: ", formattedResult);
+
+    res.json({
+      reply: formattedResult,
+    });
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.error("Error:", error);
+
+    res.status(500).send("Internal Server Error");
+  }
+  return;
 };
